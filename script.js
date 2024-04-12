79689d513ef4293aa8e9621bbee862d0f773c308
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-04-11 23:25:25 -0700

    I add the code for the button caps

--- script.js
+++ script.js
@@ -14,12 +14,40 @@ userWords.forEach(function (value) {
     console.log(value);
     const wordListItem = document.createElement("li");
     const wordList = document.getElementById("wordList");
-    const textConvert = document.createTextNode(value);
+    const textNode = document.createTextNode(value);
 
-    wordListItem.appendChild(textConvert);
+    wordListItem.appendChild(textNode);
     wordList.appendChild(wordListItem);
 });
 
  const contentPage = document.getElementById("wrapper");
  contentPage.classList.remove("hidden");
+
+function btnCaps() {
+    const userWordsCaps = userWords.map(function (wordByUser) {
+        let capsWord = wordByUser.toUpperCase();
+        return capsWord; 
+    })
+    console.log(userWordsCaps);
+    let wordList = document.getElementById("wordList");
+    wordList.innerHTML = "";
+
+    userWordsCaps.forEach(function (capsValue){
+        const capsNode = document.createTextNode(capsValue);
+        const wordListItem = document.createElement("li");
+        const wordList = document.getElementById("wordList");
+        
+        wordListItem.appendChild(capsNode);
+        wordList.appendChild(wordListItem);
+    });
+}
+
+function btnRed() {
+    const userWordRed = user
+}
+
+
+
+ 
+
 
