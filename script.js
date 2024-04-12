5b77d4a9c40e5e769b582d1e53927f103932daf9
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-04-11 21:12:26 -0700

    functions to create the li and to change to a node

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const userWords = [];
 
 while (userWords.length < 3) {
     const word = prompt("Please enter your words!");
-    debugger;
+
     if (!word) {
         alert("You need to type something");
     } else {
@@ -10,4 +10,16 @@ while (userWords.length < 3) {
     }
 }
 
+userWords.forEach(function (value) {
+    console.log(value);
+    const wordListItem = document.createElement("li");
+    const wordList = document.getElementById("wordList");
+    const textConvert = document.createTextNode(value);
+
+    wordListItem.appendChild(textConvert);
+    wordList.appendChild(wordListItem);
+});
+
+ const contentPage = document.getElementById("wrapper");
+ contentPage.classList.remove("hidden");
 
