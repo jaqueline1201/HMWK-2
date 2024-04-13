38608f700cb621ec41f51ee4ca68c1b8ee9e4f63
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-04-12 18:19:37 -0700

    btn red created

--- script.js
+++ script.js
@@ -11,47 +11,49 @@ while (userWords.length < 3) {
 }
 
 renderList(userWords);
+
+const contentPage = document.getElementById("wrapper");
+ contentPage.classList.remove("hidden");
+
+const btnRed = document.getElementById("redFonts");
+btnRed.addEventListener("click", function() {
+    renderList(userWords);
+    const uList = document.getElementById("wordList");
+    uList.classList.add("redWords");
+});
+
 // The forEach() method calls a function for each element in an array.//
 function renderList (workingArray) {
+    let wordList = document.getElementById("wordList");
+    wordList.innerHTML = "";
+    wordList.classList.remove("redWords");
+
     workingArray.forEach(function (value) {
-        console.log(value);
         const wordListItem = document.createElement("li");
         const wordList = document.getElementById("wordList");
         const textNode = document.createTextNode(value);
 
         wordListItem.appendChild(textNode);
         wordList.appendChild(wordListItem);
-    });
+    }); 
 }
- const contentPage = document.getElementById("wrapper");
- contentPage.classList.remove("hidden");
-
 //map() creates a new array from calling a function for every array element.//
  function btnCaps() {
     const userWordsCaps = userWords.map(function (wordByUser) {
         let capsWord = wordByUser.toUpperCase();
         return capsWord; 
     })
-    console.log(userWordsCaps);
-    let wordList = document.getElementById("wordList");
-    wordList.innerHTML = "";
 
    renderList(userWordsCaps);
 }
-
-
 
 function btnSwap () {
     const userWordsSwap = userWords.map(function (wordByUser) {
         let swapWord = wordByUser.charAt(wordByUser.length - 1)+ wordByUser.substring(1,wordByUser.length -1 ) + wordByUser.charAt(0);
         return swapWord;
     })
-    console.log(userWordsSwap);
-    let wordList = document.getElementById("wordList");
-    wordList.innerHTML = "";
 
    renderList(userWordsSwap);
-
 }
 
 
